14f1d9e5a7981bef90f7da40629e63b550c9e336
Malikd96 <96malikdavis@gmail.com>
2024-08-20 19:14:16 -0400

    exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,23 @@
+#Arithmetic Operations in Daily Life
+#Grocery Store Math Calculate the total cost of three items 
+# you'd commonly find in a grocery store, given their individual
+#  prices. For example, what would be the cost of bread, peanut 
+# butter, and jelly be? Prices don't need to be realistic!
+
+bread_price = 2
+peanut_butter_price = 3
+jelly_price = 1
+total_cost = bread_price + peanut_butter_price + jelly_price
+print(total_cost)
+
+#Task2: Bank Interest. If you have a savings account with a 
+# particular initial amount and a fixed yearly interest rate, 
+# calculate the total amount in your account after a year. 
+# So if you had $10,000 at a 7% interest write code that would 
+# tell us the amount at the end of the year. For the example 
+# the expected outcome would be $10,700.
+
+initial_amount = 10000
+interest_amount = 0.07
+end_of_year_amount = initial_amount * (1 + interest_amount)
+print(end_of_year_amount)
